90f23a326d2f30342570050f800a7fbeb6e7a27c
jakebrowning90 <jacobbrowning90@gmail.com>
2023-02-05 17:44:23 -0500

    Add function to validate moves

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function knightMoves() {
 //Return array [start, path, end]
 }
 
-//Create 8x8 gameboard
-function makeBoard() {
+//Create 8x8 gameboard with x,y matching physical layout
+function makeSimBoard() {
     let gameboard = [];
     for (i = 0; i < 8; i++) {
         let row = [];
@@ -14,12 +14,64 @@ function makeBoard() {
             let cell = [];
             cell.push(i);
             cell.push(j);
-            row[j] = cell.reverse().toString();
+            row[j] = cell;
         }
         gameboard[i] = row;
     }
-    return gameboard.reverse();
+    return gameboard;
 }
 
-const gameboard = makeBoard();
+const gameboard = makeSimBoard();
 console.log(gameboard);
+
+//Create array of all gameboard spaces
+function makeCalcBoard() {
+    let spaceArray = [];
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            let cell = [];
+            cell.push(i);
+            cell.push(j);
+            spaceArray.push(cell)
+        }
+    }
+    return spaceArray;
+}
+
+const spaceArray = makeCalcBoard();
+//console.log(spaceArray);
+
+// Return the valid moves a knight can make from a given space
+function getPossibleMoves(cell) {
+    // Get all 8 moves a knight can possibly make
+    const nextMoves = [
+        [cell[0] + 1, cell[1] + 2],
+        [cell[0] + 2, cell[1] + 1],
+        [cell[0] + 2, cell[1] - 1],
+        [cell[0] + 1, cell[1] - 2],
+        [cell[0] - 1, cell[1] - 2],
+        [cell[0] - 2, cell[1] - 1],
+        [cell[0] - 2, cell[1] + 1],
+        [cell[0] - 1, cell[1] + 2]]
+    // Return only the valid moves
+    const validMoves = [];
+    for (let i = 0; i < nextMoves.length; i++) {
+        if (isMoveValid(nextMoves[i]) == true) {
+            validMoves.push(nextMoves[i].toString());
+        }
+    }
+    return validMoves;
+}
+
+// Check if a move lands on the board
+function isMoveValid(outcome) {
+    if (outcome[0] < 0 || outcome[0] > 7 || outcome[1] < 0 || outcome[1] > 7 ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const startingCell = gameboard[3][6];
+console.log("From "+ startingCell.toString() + ", you can go to:");
+console.log(getPossibleMoves(startingCell));
