ec0700c65cd5888e9184300c20ff1a22a802ebb0
jakebrowning90 <jacobbrowning90@gmail.com>
2023-02-06 12:41:38 -0500

    Transfer move validation functions to tree object

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-console.log("Knight time!")
-
-// Take start and end squares as arguments
-function knightMoves() {
-//Return array [start, path, end]
-}
-
 //Create 8x8 gameboard with x,y matching physical layout
 function makeSimBoard() {
     let gameboard = [];
@@ -86,7 +79,68 @@ class Node {
 
 // Tree factory
 class SearchTree {
-    constructor(square) {
-        this.root = this.buildTree(square);
+    constructor(start, end) {
+        this.root = this.buildTree(start, end);
+        this.loopingMoves = [];
+    }
+
+    buildTree(square, end){
+        let root = new Node(square);
+        root.moves = this.getPossibleMoves(square);
+        // Continue making roots if moves does not contain "end"
+        // if (root.moves.includes(end) == false) {
+        //     for (let i = 0; i < root.moves.length; i++) {
+        //         root.moves[i] = this.buildTree(root.moves[i])
+        //     }
+        // }
+        return root;
+    }
+
+    // Return the valid moves a knight can make from a given space
+    getPossibleMoves(cell) {
+        // Get all 8 moves a knight can possibly make
+        const nextMoves = [
+            [cell[0] + 1, cell[1] + 2],
+            [cell[0] + 2, cell[1] + 1],
+            [cell[0] + 2, cell[1] - 1],
+            [cell[0] + 1, cell[1] - 2],
+            [cell[0] - 1, cell[1] - 2],
+            [cell[0] - 2, cell[1] - 1],
+            [cell[0] - 2, cell[1] + 1],
+            [cell[0] - 1, cell[1] + 2]]
+        // Return only the valid moves
+        const validMoves = [];
+        for (let i = 0; i < nextMoves.length; i++) {
+            if (this.isMoveValid(nextMoves[i]) == true) {
+                validMoves.push(nextMoves[i]);
+            }
+        }
+        //this.loopingMoves.push(validMoves);
+        return validMoves;
+    }
+
+    // Check if a move lands on the board and isn't revisting a square
+    isMoveValid(outcome) {
+        if (outcome[0] < 0 || outcome[0] > 7 || outcome[1] < 0 || outcome[1] > 7) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
+
+// Take start and end squares as arguments
+function knightMoves(start, end) {
+    // Set "start" as root of new tree
+    const search = new SearchTree(start, end);
+    console.log(search.root.value);
+    console.log(search.root.moves);
+    // Get all of root's valid moves, 
+    
+    // Recurse if valid moves do not include "end"
+    
+    
+    //Return array [start, path, end]
+    }
+
+ knightMoves(gameboard[3][6], gameboard[0][0]);
